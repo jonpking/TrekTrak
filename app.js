f49a529b3a28fb71100ddd3fd6abebd507097f2e
Jonathan King <jonpking@gmail.com>
2020-08-16 16:48:05 -0700

    installed/setup method-override, added update route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 
 mongoose.connect("mongodb://localhost:27017/trek_trak", {
     useNewUrlParser: true,
@@ -23,6 +24,7 @@ const Journal = mongoose.model("Journal", journalSchema);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
 
 // ROOT ROUTE
 app.get("/", function (req, res) {
@@ -78,6 +80,17 @@ app.get("/journals/:id/edit", function (req, res) {
             console.log(err);
         } else {
             res.render("edit", { journal: foundJournal });
+        }
+    });
+});
+
+// UPDATE ROUTE
+app.put("/journals/:id", function (req, res) {
+    Journal.findByIdAndUpdate(req.params.id, req.body.journal, function (err, updatedJournal) {
+        if (err) {
+            res.redirect("/journals");
+        } else {
+            res.redirect("/journals/" + req.params.id);
         }
     });
 });
