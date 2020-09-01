a40604f8065f1dc16e1b8d0c932c3352a0290a8b
Jonathan King <jonpking@gmail.com>
2020-08-31 23:46:55 -0700

    added functions for checking comment and journal ownership to the middlewareObj

--- middleware/index.js
+++ middleware/index.js
@@ -10,4 +10,40 @@ middlewareObj.isLoggedIn = function (req, res, next) {
     res.redirect("/login");
 }
 
+middlewareObj.checkJournalOwnership = function (req, res, next) {
+    if (req.isAuthenticated()) {
+        Journal.findById(req.params.id, function (err, foundJournal) {
+            if (err) {
+                res.redirect("back");
+            } else {
+                if (foundJournal.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        res.redirect("back");
+    }
+}
+
+middlewareObj.checkCommentOwnership = function (req, res, next) {
+    if (req.isAuthenticated()) {
+        Comment.findById(req.params.comment_id, function (err, foundComment) {
+            if (err) {
+                res.redirect("back");
+            } else {
+                if (foundComment.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        res.redirect("back");
+    }
+}
+
 module.exports = middlewareObj;
