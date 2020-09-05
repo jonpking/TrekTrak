4a9f434f14d6ae7cf3a87db98feeb128806b3434
Jonathan King <jonpking@gmail.com>
2020-09-04 20:01:13 -0700

    updated registration validators

--- routes/index.js
+++ routes/index.js
@@ -35,32 +35,26 @@ router.get("/register", function (req, res) {
 
 // REGISTER LOGIC
 router.post("/register", function (req, res) {
+    // REGISTERED USERNAME VALIDATION
+    if (!validator.isLength(req.body.username, { min: 4, max: 20 })) {
+        req.flash("error", "Username must be between 4 and 20 characters");
+        return res.redirect("/register");
+    }
+    if (!validator.isAlphanumeric(req.body.username)) {
+        req.flash("error", "Username must be alphanumeric");
+        return res.redirect("/register");
+    }
+    // REGISTERED PASSWORD VALIDATION
+    if (!validator.isLength(req.body.password, { min: 8, max: 64 })) {
+        req.flash("error", "Password must be between 8 and 64 characters");
+        return res.redirect("/register");
+    }
     const newUser = new User({ username: req.body.username });
     User.register(newUser, req.body.password, function (err, user) {
-        // REGISTERED USERNAME VALIDATION
-        if (!validator.isLength(req.body.username, { min: 1, max: 5 })) {
-            req.flash("error", "Username must be between 1 and 5 characters");
-            return res.redirect("/register");
-        }
-        if (!validator.isAlphanumeric(req.body.username)) {
-            req.flash("error", "Username must be alphanumeric");
-            return res.redirect("/register");
-        }
-        // REGISTERED PASSWORD VALIDATION
-        if (!validator.isLength(req.body.password, { min: 1, max: 5 })) {
-            req.flash("error", "Password must be between 1 and 5 characters");
-            return res.redirect("/register");
-        }
-        if (!validator.isAlphanumeric(req.body.password)) {
-            req.flash("error", "Password must be alphanumeric");
-            return res.redirect("/register");
-        }
-        // CHECK REGISTRATION FOR ERROR
         if (err) {
             req.flash("error", err.message);
             return res.redirect("/register");
         }
-        // IF ALL TESTS PASS, PROCESS REGISTRATION
         passport.authenticate("local")(req, res, function () {
             req.flash("success", "Welcome to TrekTrak " + user.username);
             res.redirect("/journals");
