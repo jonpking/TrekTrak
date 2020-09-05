85de582ea9048526b6d0bfdecc1fa7a94fd78cb4
Jonathan King <jonpking@gmail.com>
2020-09-04 18:56:47 -0700

    started setting up validation for username/password registration

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
+const validator = require("validator");
 
 const User = require("../models/user");
 
@@ -36,10 +37,30 @@ router.get("/register", function (req, res) {
 router.post("/register", function (req, res) {
     const newUser = new User({ username: req.body.username });
     User.register(newUser, req.body.password, function (err, user) {
+        // REGISTERED USERNAME VALIDATION
+        if (!validator.isLength(req.body.username, { min: 1, max: 5 })) {
+            req.flash("error", "Username must be between 1 and 5 characters");
+            return res.redirect("/register");
+        }
+        if (!validator.isAlphanumeric(req.body.username)) {
+            req.flash("error", "Username must be alphanumeric");
+            return res.redirect("/register");
+        }
+        // REGISTERED PASSWORD VALIDATION
+        if (!validator.isLength(req.body.password, { min: 1, max: 5 })) {
+            req.flash("error", "Password must be between 1 and 5 characters");
+            return res.redirect("/register");
+        }
+        if (!validator.isAlphanumeric(req.body.password)) {
+            req.flash("error", "Password must be alphanumeric");
+            return res.redirect("/register");
+        }
+        // CHECK REGISTRATION FOR ERROR
         if (err) {
             req.flash("error", err.message);
             return res.redirect("/register");
         }
+        // IF ALL TESTS PASS, PROCESS REGISTRATION
         passport.authenticate("local")(req, res, function () {
             req.flash("success", "Welcome to TrekTrak " + user.username);
             res.redirect("/journals");
